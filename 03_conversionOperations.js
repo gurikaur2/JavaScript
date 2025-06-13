addbfcc68dfeb6b259f36d0ad1065ad4edaa79f8
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-06-13 15:52:21 +0530

    Initial commit

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,7 +1,30 @@
-let score="44"
+/* let score="45"
 
 console.log(typeof score);
 console.log(typeof (score));
+
+let valueInNum= Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum); */
+
+
+
+/* let isLoggedIn="guri";
+let boolIsLoggedIn=Boolean(isLoggedIn);
+console.log(boolIsLoggedIn);
+
+ */
+
+
+let someNum=77;
+console.log("Before conversion:",typeof someNum);
+
+let stringValue=String(someNum);
+console.log("After conversion:",typeof stringValue);
+
+console.log(stringValue);
+
+
 
 
 
