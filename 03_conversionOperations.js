03970047904f69a7124859b59fa7f8598209ad01
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-06-14 22:59:30 +0530

    Initial commit

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -16,13 +16,56 @@ console.log(boolIsLoggedIn);
  */
 
 
-let someNum=77;
+/* let someNum=77;
 console.log("Before conversion:",typeof someNum);
 
 let stringValue=String(someNum);
 console.log("After conversion:",typeof stringValue);
 
-console.log(stringValue);
+console.log(stringValue); */
+
+
+//OPERATIONS
+
+let value=3;
+let negValue= -value;
+/* console.log("Original value:",value);
+console.log("Negative value:",negValue); */
+ 
+/* console.log(2+2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**4); */
+
+let str1="Hi ";
+let str2="Guri";
+let str3=str1 + str2;
+//console.log(str3);
+
+console.log("1"+2);
+console.log(1+2+"yoo");
+console.log(1+"2"+2);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+ 
+
+
+
 
 
 
