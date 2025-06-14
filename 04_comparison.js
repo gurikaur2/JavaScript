a7c69ea08baa40d25f441f34bba930cb7d104fed
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-06-14 23:29:31 +0530

    Initial commit

---
+++ 04_comparison.js
@@ -0,0 +1,29 @@
+/* console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+ */
+
+//console.log("2">1);
+
+/* console.log(null>0);
+console.log(null>=0);
+console.log(null==0); */
+
+
+/* console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined==0); */
+
+//===  is strict check
+
+console.log("2"==2);
+console.log("2"===2); //datatype is also checked
+
+
+
+
+
+
+
+
+
